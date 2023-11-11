d170c6c443388e48a1b053fd5cf5b84713f7c14c
juanpablopnetgrid <47440697+juanpabloperez98@users.noreply.github.com>
2023-11-11 12:21:19 -0500

    Adding poke api exercise

---
+++ index.js
@@ -0,0 +1,28 @@
+async function searchPokemon(){
+    let poke=document.getElementById("pokemonInput").value
+    poke = poke.toLowerCase()
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
+        const data = await response.json();
+        console.log(data);
+        displayPokemonInfo(data)
+    } catch (error) {
+        displayError();
+    }
+}
+
+function displayPokemonInfo(pokeData) {
+    const pokemonInfoElement = document.getElementById('pokemonInfo');
+        pokemonInfoElement.innerHTML = `
+        <h2>Nombre: ${pokeData.name}</h2>
+        <img src="${pokeData.sprites.front_default}" alt="${pokeData.name}">
+        <p>Altura: ${pokeData.height}</p>
+        <p>Ancho: ${pokeData.weight}</p>
+        `
+}
+
+function displayError() {
+    const pokemonInfoElement = document.getElementById('pokemonInfo');
+    pokemonInfoElement.innerHTML = `<p>No se encontro información del pokemon</p>`
+}
+
